6fa335941dd30b2d507193d1c209b5cd5baf028c
Jamie Michael <jamie.michael@capita.com>
2020-09-12 13:00:59 +0100

    Deleted unused code

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,10 +1,4 @@
 import requests 
-#from flask import session
-
-_DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
-]
 
 def get_token():
     f = open("todo_app\\data\\key.gitignore", "r")
@@ -44,21 +38,6 @@ def get_items():
 
     return response.json()
 
-
-def get_item(id):
-    """
-    Fetches the saved item with the specified ID.
-
-    Args:
-        id: The ID of the item.
-
-    Returns:
-        item: The saved item, or None if no items match the specified ID.
-    """
-    items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
-
-
 def add_item(title):
     """
     Adds a new item with the specified title to the session.
@@ -86,21 +65,6 @@ def add_item(title):
     url,
     params=query
     )
-
-def save_item(item):
-    """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
-
-    Args:
-        item: The item to save.
-    """
-    existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
-    session['items'] = updated_items
-
-    return item
-
 
 def complete_item(id):
     apikey = get_key()
